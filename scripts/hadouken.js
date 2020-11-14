dd9a91a2ca751b2ab45d4b6a36d3f8bf9b61dd92
Ignacio Tomas Crespo <itcrespo@gmail.com>
2020-11-14 02:54:32 +0100

    eyes draft

--- scripts/hadouken.js
+++ scripts/hadouken.js
@@ -89,7 +89,7 @@ function onFaceDetected(face){
 //    drawEye(face.annotations.rightEyeUpper0);
 //    drawEye(face.annotations.rightEyeUpper1);
 //    drawEye(face.annotations.rightEyeUpper2);
-    drawEye(face.annotations.rightEyeLower0);
+//    drawEye(face.annotations.rightEyeLower0);
 //    drawEye(face.annotations.rightEyeLower1);
 //    drawEye(face.annotations.rightEyeLower2);
 //    drawEye(face.annotations.rightEyeLower3);
@@ -104,17 +104,129 @@ function clearFaces() {
     fcontext.clearRect(0, 0, fca.width, fca.height);
 }
 
+var stage;
+var particles = [];
+var max = 60;
+var mouseX=0;
+var mouseY=0;
+
+var speed=3;
+var size=5;
+
+//The class we will use to store particles. It includes x and y
+//coordinates, horizontal and vertical speed, and how long it's
+//been "alive" for.
+function Particle(x, y, xs, ys) {
+  this.x=x;
+  this.y=y;
+  this.xs=xs;
+  this.ys=ys;
+  this.life=0;
+}
+
+//function resizeCanvas() {
+//  setTimeout(function() {
+//    width = window.innerWidth;
+//    height = window.innerHeight;
+//    canvas.width = width;
+//    canvas.height = height;
+//    canvas.style.width = width + "px";
+//    canvas.style.height = height + "px";
+//    mouseX=canvas.width/2;
+//    mouseY=canvas.height*0.8;
+//   stage.globalCompositeOperation="lighter"
+//  }, 0);
+//}
+
+function init_fire() {
+
+  //See if the browser supports canvas
+    //Get the canvas context to draw onto
+    stage = fcontext;
+
+    //Makes the colors add onto each other, producing
+    //that nice white in the middle of the fire
+    stage.globalCompositeOperation="xor";
+
+//    window.addEventListener("resize", function() {
+//      resizeCanvas();
+      stage.globalCompositeOperation="lighter";
+//      mouseX=canvas.width/2;
+//      mouseY=canvas.height*0.8;
+//    });
+
+    //Update the particles every frame
+//    var timer=setInterval(update,40);
+
+}
+
+//function getMousePos (evt) {
+//  var rect = canvas.getBoundingClientRect();
+//  var root = document.documentElement;
+//
+//   return mouse position relative to the canvas
+//  mouseX = evt.clientX - rect.left - root.scrollLeft;
+//  mouseY = evt.clientY - rect.top - root.scrollTop;
+//}
+
+function update_fire() {
+
+  //Adds ten new particles every frame
+  for (var i=0; i<10; i++) {
+
+    if(isDrawingFluids) {
+        //Adds a particle at the mouse position, with random horizontal and vertical speeds
+        var p = new Particle(mouseX, mouseY, (Math.random()*2*speed-speed)/2, 0-Math.random()*2*speed);
+        particles.push(p);
+    }
+  }
+
+  //Clear the stage so we can draw the new frame
+  clearFaces();
+
+  //Cycle through all the particles to draw them
+  for (i=0; i<particles.length; i++) {
+
+    //Set the file colour to an RGBA value where it starts off red-orange, but progressively gets more grey and transparent the longer the particle has been alive for
+    stage.fillStyle = "rgba("+(260-(particles[i].life*2))+","+((particles[i].life*2)+50)+","+(particles[i].life*2)+","+(((max-particles[i].life)/max)*0.4)+")";
+
+    stage.beginPath();
+    //Draw the particle as a circle, which gets slightly smaller the longer it's been alive for
+    stage.arc(particles[i].x,particles[i].y,(max-particles[i].life)/max*(size/2)+(size/2),0,2*Math.PI);
+    stage.fill();
+
+    //Move the particle based on its horizontal and vertical speeds
+    particles[i].x+=particles[i].xs;
+    particles[i].y+=particles[i].ys;
+
+    particles[i].life++;
+    //If the particle has lived longer than we are allowing, remove it from the array.
+    if (particles[i].life >= max) {
+      particles.splice(i, 1);
+      i--;
+    }
+  }
+}
+
+init_fire();
+
 function drawEye(eye) {
-    try{
+//    try{
     //    console.log("face: "+ eye);
         var posx = parseInt(eye[4][0]);
         var posy = parseInt(eye[4][1]);
+
     //    console.log("face X,Y: "+ posx +","+posy);
-        fcontext.beginPath();
-        fcontext.arc(posx, posy, 2, 0, 2 * Math.PI);
-        fcontext.stroke();
-        fcontext.fill();
-    }catch(e){}
+//        fcontext.beginPath();
+//        fcontext.arc(posx, posy, 2, 0, 2 * Math.PI);
+//        fcontext.stroke();
+//        fcontext.fill();
+
+        mouseX = posx;
+        mouseY = posy;
+        update_fire();
+
+//    }catch(e){}
 }
 
 function onHandDetected(prediction){
@@ -136,6 +248,8 @@ function onHandDetected1(prediction){
     }
 }
 
+var isDrawingFluids = hadouken.handsDetectionEngine == 1 ? true : false;
+
 // https://github.com/tensorflow/tfjs-models/tree/master/handpose
 //                thumb: [1, 2, 3, 4],
 //                indexFinger: [5, 6, 7, 8],
@@ -144,6 +258,7 @@ function onHandDetected1(prediction){
 //                pinky: [17, 18, 19, 20],
 //                palmBase: [0]
 function onHandDetected2(prediction){
+    isDrawingFluids = false;
     if(prediction.handInViewConfidence > 0.9) {
         // draw fluids in base middle finger
         var posX = scaleByPixelRatio(prediction.annotations.middleFinger[0][0]);
@@ -157,6 +272,7 @@ function onHandDetected2(prediction){
 //            && isFingerOpen(prediction.annotations.pinky)
         ){
             if(posX > 0 && posY > 0) {
+                isDrawingFluids = true;
 //                console.log("Predictions: ", posX +","+ posY);
                 drawFluid(posX, posY);
             }
