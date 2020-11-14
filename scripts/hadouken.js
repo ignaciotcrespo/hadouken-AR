4be9c0cb699de9360e80d52439fbb443ec05eb80
Ignacio Tomas Crespo <itcrespo@gmail.com>
2020-11-14 03:13:08 +0100

    eyes working

--- scripts/hadouken.js
+++ scripts/hadouken.js
@@ -72,6 +72,8 @@ function handsDetectorVideoInitialized(width, height) {
 var fca = document.getElementById("canvas_face");
 var fcontext = fca.getContext("2d");
 fcontext.fillStyle = "#FF0000";
+var fire1 = new FireEye();
+var fire2 = new FireEye();
 
 function onFaceDetected(face){
     fcontext.clearRect(0, 0, fca.width, fca.height);
@@ -79,7 +81,7 @@ function onFaceDetected(face){
 //    drawEye(face.annotations.leftEyeUpper0);
 //    drawEye(face.annotations.leftEyeUpper1);
 //    drawEye(face.annotations.leftEyeUpper2);
-    drawEye(face.annotations.leftEyeLower0);
+    drawEye1(face.annotations.leftEyeLower0);
 //    drawEye(face.annotations.leftEyeLower1);
 //    drawEye(face.annotations.leftEyeLower2);
 //    drawEye(face.annotations.leftEyeLower3);
@@ -89,7 +91,7 @@ function onFaceDetected(face){
 //    drawEye(face.annotations.rightEyeUpper0);
 //    drawEye(face.annotations.rightEyeUpper1);
 //    drawEye(face.annotations.rightEyeUpper2);
-//    drawEye(face.annotations.rightEyeLower0);
+    drawEye2(face.annotations.rightEyeLower0);
 //    drawEye(face.annotations.rightEyeLower1);
 //    drawEye(face.annotations.rightEyeLower2);
 //    drawEye(face.annotations.rightEyeLower3);
@@ -104,14 +106,59 @@ function clearFaces() {
     fcontext.clearRect(0, 0, fca.width, fca.height);
 }
 
-var stage;
-var particles = [];
-var max = 60;
-var mouseX=0;
-var mouseY=0;
+function FireEye() {
+    this.particles = [];
+    this.max = 60;
+    this.mouseX=0;
+    this.mouseY=0;
 
-var speed=3;
-var size=5;
+    this.speed=3;
+    this.size=5;
+
+    this.update_fire = function() {
+
+        var stage = fcontext;
+        var particles = this.particles;
+        var max = this.max;
+        var size = this.size;
+
+      //Adds ten new particles every frame
+      for (var i=0; i<10; i++) {
+
+        if(isDrawingFluids) {
+            //Adds a particle at the mouse position, with random horizontal and vertical speeds
+            var p = new Particle(this.mouseX, this.mouseY, (Math.random() * 2 * this.speed - this.speed)/2, 0 - Math.random() * 2 * this.speed);
+            particles.push(p);
+        }
+      }
+
+      //Clear the stage so we can draw the new frame
+//      clearFaces();
+
+      //Cycle through all the particles to draw them
+      for (i=0; i<particles.length; i++) {
+
+        //Set the file colour to an RGBA value where it starts off red-orange, but progressively gets more grey and transparent the longer the particle has been alive for
+        stage.fillStyle = "rgba("+(260-(particles[i].life*2))+","+((particles[i].life*2)+50)+","+(particles[i].life*2)+","+(((max-particles[i].life)/max)*0.4)+")";
+
+        stage.beginPath();
+        //Draw the particle as a circle, which gets slightly smaller the longer it's been alive for
+        stage.arc(particles[i].x,particles[i].y,(max-particles[i].life)/max*(size/2)+(size/2),0,2*Math.PI);
+        stage.fill();
+
+        //Move the particle based on its horizontal and vertical speeds
+        particles[i].x+=particles[i].xs;
+        particles[i].y+=particles[i].ys;
+
+        particles[i].life++;
+        //If the particle has lived longer than we are allowing, remove it from the array.
+        if (particles[i].life >= max) {
+          particles.splice(i, 1);
+          i--;
+        }
+      }
+    }
+}
 
 //The class we will use to store particles. It includes x and y
 //coordinates, horizontal and vertical speed, and how long it's
@@ -124,93 +171,39 @@ function Particle(x, y, xs, ys) {
   this.life=0;
 }
 
-//function resizeCanvas() {
-//  setTimeout(function() {
-//    width = window.innerWidth;
-//    height = window.innerHeight;
-//    canvas.width = width;
-//    canvas.height = height;
-//    canvas.style.width = width + "px";
-//    canvas.style.height = height + "px";
-//    mouseX=canvas.width/2;
-//    mouseY=canvas.height*0.8;
-//   stage.globalCompositeOperation="lighter"
-//  }, 0);
-//}
+    function init_fire() {
 
-function init_fire() {
+      //See if the browser supports canvas
+        //Get the canvas context to draw onto
+        var stage = fcontext;
 
-  //See if the browser supports canvas
-    //Get the canvas context to draw onto
-    stage = fcontext;
+        //Makes the colors add onto each other, producing
+        //that nice white in the middle of the fire
+        stage.globalCompositeOperation="xor";
 
-    //Makes the colors add onto each other, producing
-    //that nice white in the middle of the fire
-    stage.globalCompositeOperation="xor";
+    //    window.addEventListener("resize", function() {
+    //      resizeCanvas();
+          stage.globalCompositeOperation="lighter";
+    //      mouseX=canvas.width/2;
+    //      mouseY=canvas.height*0.8;
+    //    });
 
-//    window.addEventListener("resize", function() {
-//      resizeCanvas();
-      stage.globalCompositeOperation="lighter";
-//      mouseX=canvas.width/2;
-//      mouseY=canvas.height*0.8;
-//    });
+        //Update the particles every frame
+    //    var timer=setInterval(update,40);
 
-    //Update the particles every frame
-//    var timer=setInterval(update,40);
-
-}
-
-//function getMousePos (evt) {
-//  var rect = canvas.getBoundingClientRect();
-//  var root = document.documentElement;
-//
-//   return mouse position relative to the canvas
-//  mouseX = evt.clientX - rect.left - root.scrollLeft;
-//  mouseY = evt.clientY - rect.top - root.scrollTop;
-//}
-
-function update_fire() {
-
-  //Adds ten new particles every frame
-  for (var i=0; i<10; i++) {
-
-    if(isDrawingFluids) {
-        //Adds a particle at the mouse position, with random horizontal and vertical speeds
-        var p = new Particle(mouseX, mouseY, (Math.random()*2*speed-speed)/2, 0-Math.random()*2*speed);
-        particles.push(p);
     }
-  }
-
-  //Clear the stage so we can draw the new frame
-  clearFaces();
-
-  //Cycle through all the particles to draw them
-  for (i=0; i<particles.length; i++) {
-
-    //Set the file colour to an RGBA value where it starts off red-orange, but progressively gets more grey and transparent the longer the particle has been alive for
-    stage.fillStyle = "rgba("+(260-(particles[i].life*2))+","+((particles[i].life*2)+50)+","+(particles[i].life*2)+","+(((max-particles[i].life)/max)*0.4)+")";
-
-    stage.beginPath();
-    //Draw the particle as a circle, which gets slightly smaller the longer it's been alive for
-    stage.arc(particles[i].x,particles[i].y,(max-particles[i].life)/max*(size/2)+(size/2),0,2*Math.PI);
-    stage.fill();
-
-    //Move the particle based on its horizontal and vertical speeds
-    particles[i].x+=particles[i].xs;
-    particles[i].y+=particles[i].ys;
-
-    particles[i].life++;
-    //If the particle has lived longer than we are allowing, remove it from the array.
-    if (particles[i].life >= max) {
-      particles.splice(i, 1);
-      i--;
-    }
-  }
-}
 
 init_fire();
 
-function drawEye(eye) {
+function drawEye1(eye) {
+    drawEye(eye, fire1);
+}
+
+function drawEye2(eye) {
+    drawEye(eye, fire2);
+}
+
+function drawEye(eye, fire) {
 //    try{
     //    console.log("face: "+ eye);
         var posx = parseInt(eye[4][0]);
@@ -222,9 +215,9 @@ function drawEye(eye) {
 //        fcontext.stroke();
 //        fcontext.fill();
 
-        mouseX = posx;
-        mouseY = posy;
-        update_fire();
+        fire.mouseX = posx;
+        fire.mouseY = posy;
+        fire.update_fire();
 
 //    }catch(e){}
 }
